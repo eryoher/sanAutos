0018e7cb84791b97c018196456e361f98fc73010
Sergio Portela <sergio.portela@myprocess.net>
2019-08-10 11:38:37 -0500

    Ajustes de diseño

--- components/common/footer.js
+++ components/common/footer.js
@@ -17,9 +17,83 @@ export default class Footer extends Component {
         return (
             <Col span={24} className={"footer-cont"} >
                 <Col span={24} className={"footer-term"} >
-                    Aplica desde el 10 de Agosto hasta el 25 de Agosto de 2019 y únicamente para la compra de vehículos nuevos particulares de la marca Renault Nuevo Logan, Nuevo Sandero, Nuevo Stepway, Duster, Oroch, Captur, Koleos y Alaskan modelo 2019 y 2020, vendidos a través de la red de concesionarios Renault en la ciudad de Bogotá, financiados a través de RCI Colombia S.A Compañía de Financiamiento. El cliente tendrá un período de gracia para el abono de capital, pago de intereses y pago de cargos adicionales (seguros contratados por el cliente, más el valor del registro de garantía mobiliaria que se cobra en la primera cuota del crédito y los gastos generados por la pre-inscripción de la garantía ante el RUNT) durante las cinco (5) primeras cuotas del crédito. La tasa de interés durante el período de gracia es del 0% E.A. Terminado el periodo de gracia, el cliente paga cuotas mensuales compuestas de abono a capital, pago de intereses y pago de cargos adicionales (seguros contratados por el cliente más el valor de la cancelación de la garantía mobiliaria y del levantamiento de prenda, las cuales se cobran en la última cuota del crédito, y los gastos generados por la cancelación de la garantía ante el RUNT, los cuales se cobran durante la vigencia del crédito por una sola vez, entre otros). El plazo del crédito es único de 72 meses incluido el periodo de gracia. La tasa de interés del período posterior al periodo de gracia será según la política de tasas vigente de la entidad que financia (Tasa variable indexada a la DTF). El plazo es estimado y puede extenderse debido a la variación de la tasa y la modalidad de cuota fija. El valor financiado para vehículos Logan y Sandero será hasta el 70% del valor de la factura de venta del vehículo, para vehículo Stepway  será hasta el 60% del valor de la factura de venta  y  para el resto de la gama de vehículos será hasta el 50% del valor de la factura de venta del vehículo. La aprobación está sujeta a estudio de crédito y cumplimiento de la política de riesgo de la entidad que financia. “5 meses 0 Pesos” hace referencia al no pago de la cuota del crédito durante el periodo de gracia en virtud de la oferta comercial establecida. Los accesorios en caso de que sean financiados, son financiados dentro del crédito bajo un plan de amortización igual al del capital principal del crédito y con una tasa de interés variable indexada a la DTF igual a la del capital principal del crédito. La garantía extendida, matrícula y contrato de mantenimiento en caso de que sean financiados, son financiados dentro del crédito bajo un plan de amortización tradicional con pagos mensuales a partir del mes siguiente al desembolso y con una tasa de interés fija independiente a la del capital principal del crédito. Entidad que financia RCI COLOMBIA S.A. COMPAÑÍA DE FINANCIAMIENTO, entidad vigilada por la Superintendencia Financiera de Colombia.
-                        <br></br>
-                    Esta promoción NO aplica para otras versiones de vehículos, Sandero RS, servicio especial, flotas de proximidad, ventas corporativas y persona jurídica. Las restricciones mencionadas podrán ser consultadas en los concesionarios de la Red Autorizada Renault a nivel nacional. No es acumulable con descuentos ni otros beneficios por tarjeta MY Renault.
+                CYBER DAYS RENAULT PLAN 5 MESES 0 PESOS. Aplica desde el 10 de Agosto hasta el 25 de
+Agosto de 2019 y únicamente para la compra de vehículos nuevos particulares de la marca
+Renault Nuevo Logan, Nuevo Sandero, Nuevo Stepway, Duster, Oroch, Captur, Koleos y
+Alaskan modelo 2019 y 2020, vendidos a través de la red de concesionarios Renault en la
+ciudad de Bogotá, financiados a través de RCI Colombia S.A Compañía de Financiamiento.
+El cliente tendrá un período de gracia para el abono de capital, pago de intereses y pago
+de cargos adicionales (seguros contratados por el cliente, más el valor del registro de
+garantía mobiliaria que se cobra en la primera cuota del crédito y los gastos generados por
+la pre-inscripción de la garantía ante el RUNT) durante las cinco (5) primeras cuotas del
+crédito. La tasa de interés durante el período de gracia es del 0% E.A. Terminado el
+periodo de gracia, el cliente paga cuotas mensuales compuestas de abono a capital, pago
+de intereses y pago de cargos adicionales (seguros contratados por el cliente más el valor
+de la cancelación de la garantía mobiliaria y del levantamiento de prenda, las cuales se
+cobran en la última cuota del crédito, y los gastos generados por la cancelación de la
+garantía ante el RUNT, los cuales se cobran durante la vigencia del crédito por una sola
+vez, entre otros). El plazo del crédito es único de 72 meses incluido el periodo de gracia. La
+tasa de interés del período posterior al periodo de gracia será según la política de tasas
+vigente de la entidad que financia (Tasa variable indexada a la DTF). El plazo es estimado y
+puede extenderse debido a la variación de la tasa y la modalidad de cuota fija. El valor
+financiado para vehículos Logan y Sandero será hasta el 70% del valor de la factura de
+venta del vehículo, para vehículo Stepway será hasta el 60% del valor de la factura de
+venta y para el resto de la gama de vehículos será hasta el 50% del valor de la factura de
+venta del vehículo. La aprobación está sujeta a estudio de crédito y cumplimiento de la
+política de riesgo de la entidad que financia. “5 meses 0 Pesos” hace referencia al no pago
+de la cuota del crédito durante el periodo de gracia en virtud de la oferta comercial
+establecida. Los accesorios en caso de que sean financiados, son financiados dentro del
+crédito bajo un plan de amortización igual al del capital principal del crédito y con una tasa
+de interés variable indexada a la DTF igual a la del capital principal del crédito. La garantía
+extendida, matrícula y contrato de mantenimiento en caso de que sean financiados, son
+financiados dentro del crédito bajo un plan de amortización tradicional con pagos
+mensuales a partir del mes siguiente al desembolso y con una tasa de interés fija
+independiente a la del capital principal del crédito. Entidad que financia RCI COLOMBIA
+S.A. COMPAÑÍA DE FINANCIAMIENTO, entidad vigilada por la Superintendencia Financiera
+de Colombia. Esta promoción NO aplica para otras versiones de vehículos, Sandero RS,
+servicio especial, flotas de proximidad, ventas corporativas y persona jurídica. Las
+restricciones mencionadas podrán ser consultadas en los concesionarios de la Red
+Autorizada Renault a nivel nacional. No es acumulable con descuentos ni otros beneficios
+por tarjeta MY Renault. PÓLIZA TODO RIESGO POR DOS AÑOS: Este plan incluye seguro
+todo riesgo para el vehículo adquirido. Dicho seguro es contratado con Seguros Generales
+Suramericana S.A., Seguro con cobertura de responsabilidad civil – límite único combinado
+hasta 2.040 Millones para daños a bienes de terceros, muerte o lesión a una persona
+natural y muerte o lesión a dos o más personas naturales. El beneficio incluye cobertura
+para accesorios hasta por 20% del valor asegurado del vehículo. Para las pólizas con
+vigencia a dos años, el valor a indemnizar para las pérdidas totales será el valor factura
+
+durante el primer año y al valor fasecolda el año siguiente. La vigencia de la póliza todo
+riesgo inicia en el momento en que RCI COLOMBIA S.A Compañía de Financiamiento
+autoriza la entrega del vehículo. Antes de su vencimiento se realizará la gestión de
+renovación a través del intermediario de seguros que tiene RCI destinado para ello. Cada
+renovación se realizará por un periodo de 365 días. En caso de no renovar la póliza todo
+riesgo con RCI Colombia S.A. Compañía de Financiamiento, el cliente deberá aceptar las
+condiciones de endoso de RCI Colombia S.A. Compañía de Financiamiento y presentar la
+carátula de la póliza de automóvil endosada. Si el cliente no presenta la carátula de la
+nueva póliza con mínimo 30 días antes del vencimiento de la póliza, autoriza a RCI
+Colombia S.A Compañía de Financiamiento a mantener el vehículo financiado dentro la
+póliza colectiva que RCI Colombia S.A Compañía de Financiamiento tiene contratada por
+cuenta de sus deudores. Lo anterior implica que EL CLIENTE acepta expresamente que el
+costo de la prima de seguro correspondiente, será incluido dentro de la cuota periódica de
+la obligación crediticia. Permanencia: El cliente conoce y acepta expresamente que la
+póliza obsequiada irá ligada exclusivamente al crédito otorgado por RCI Colombia S.A.
+Compañía de Financiamiento. En virtud de lo anterior, en caso de presentarse la
+cancelación del crédito o en caso que el cliente entre en estado de cobro jurídico, se
+revocará de manera automática la póliza mencionada. Seguros Generales Suramericana
+S.A asume exclusivamente la responsabilidad del cumplimiento de las obligaciones del
+seguro frente al consumidor financiero. Este producto es ofrecido por la red de RCI
+COLOMBIA S.A COMPAÑÍA DE FINANCIAMIENTO limitándose única y exclusivamente al
+correcto cumplimiento de las instrucciones debidamente impartidas por Seguros
+Generales Suramericana S.A para la prestación del servicio en dicha red. El obsequio de la
+póliza de vehículo está sujeto a la aceptación del riesgo y condiciones de asegurabilidad
+por parte de Seguros Generales Suramericana S.A. Clientes pre-aprobados: Todos aquellos
+clientes que han sido seleccionados para la recepción de una pre-aprobación de crédito
+durante el mes de Agosto de 2019 y que han recibido comunicación sobre la pre-
+aprobación mencionada. La pre-aprobación tendrá vigencia hasta el 31 de Agosto de
+2019. Las aprobaciones definitivas están sujetas al estudio de crédito. Bono de $1.000.000
+aplica únicamente para los modelos Logan intens, Sandero AT y stepway intens. Bono de
+$1.200.000 aplica para Duster (excepto ZEN), Captur y Kangoo), Bono de $1.500.000 aplica
+para Koleos y Oroch y Bono de $2.100.000 aplica para Alaskan.
                 </Col>
             </Col>
         )
